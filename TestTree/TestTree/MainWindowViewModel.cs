12c534ab1142db8e11ac7d5db7e738c6851f195f
Elena <a.lena.chur@gmail.com>
2018-02-28 09:26:01 +0300

    TestTree project. Get data from DB table Tasks. Show tasks using TreeView (in a TextBlock).

---
+++ TestTree/TestTree/MainWindowViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestTree.Model;
+using System.Collections.ObjectModel;
+
+namespace TestTree
+{
+    public class MainWindowViewModel
+    {
+        public ObservableCollection<TreeNode> Tree { get; set; }
+        private void GenerateTree()
+        {
+            using (TaskManagmentDBEntities ctx = new TaskManagmentDBEntities())
+            {
+                //Генерируется список смежности. Корни в коллекции Tree.
+                //Словарь используется для быстрого доступа к узлам по идентификатору задачи.
+                Tree = new ObservableCollection<TreeNode>();
+                Dictionary<System.Guid, TreeNode> taskNodes = new Dictionary<Guid, TreeNode>();
+
+                //В таблице ссылка на родителя может содержать идентификатор на задачу, 
+                //которая еще не встречалась в таблице при последовательном чтении.
+                //В таком случае создается узел с пустым значением задачи, которая заполнится, когда задача встретится.
+                //Это делает возможным ошибки вида ссылки на несуществующую задачу, но пусть они будут отлавливаться при удалении или записи.
+               
+                foreach (Model.Task task in ctx.Tasks)
+                {
+                    System.Guid id = (System.Guid)task.TaskID;
+                    if (!taskNodes.ContainsKey(id))
+                        taskNodes.Add(id, new TreeNode(task));
+                    else
+                        taskNodes[id].Task = task;
+
+                    if (task.ParentTaskID == null)
+                        Tree.Add(taskNodes[id]); //Это корень
+                    else
+                    {
+                        System.Guid parentId = (System.Guid)task.ParentTaskID;
+                        if (!taskNodes.ContainsKey(parentId))
+                            taskNodes.Add(parentId, new TreeNode());
+                        taskNodes[parentId].TreeNodes.Add(taskNodes[id]);
+                        taskNodes[id].ParentNode = taskNodes[parentId];
+                    }
+                }
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            GenerateTree();
+        }
+    }
+}
---
+++ TestTree/TestTree/Model/TreeNode.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace TestTree.Model
+{
+    public class TreeNode
+    {
+        public Model.Task Task {get; set;} 
+
+        public TreeNode ParentNode { get; set; }
+        public ObservableCollection<TreeNode> TreeNodes { get; set; }
+
+        public TreeNode()
+        {
+            TreeNodes = new ObservableCollection<TreeNode>();
+        }
+        public TreeNode(Model.Task task) : this()
+        {
+            Task = task;
+        }
+        
+    }
+}
