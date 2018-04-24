a8e7d29ede800f4f3cbcabe73abe43c3b724af4d
Elena <a.lena.chur@gmail.com>
2018-04-24 21:13:00 +0300

    Добавлены классы разных работ

--- DB_scripts/WorkTypes_INSERT.sql
+++ DB_scripts/WorkTypes_INSERT.sql
@@ -2,7 +2,7 @@ USE[TaskManagmentDB]
 GO
 
 INSERT INTO [dbo].[WorkTypes] ([ID], [TypeName]) VALUES (0, 'None')
-INSERT INTO [dbo].[WorkTypes] ([ID], [TypeName]) VALUES (1, 'Error')
-INSERT INTO [dbo].[WorkTypes] ([ID], [TypeName]) VALUES (2, 'Refractoring')
-INSERT INTO [dbo].[WorkTypes] ([ID], [TypeName]) VALUES (3, 'ConsultationsByPhone')
-INSERT INTO [dbo].[WorkTypes] ([ID], [TypeName]) VALUES (4, 'Patch')
+INSERT INTO [dbo].[WorkTypes] ([ID], [TypeName]) VALUES (1, 'Ошибка ')
+INSERT INTO [dbo].[WorkTypes] ([ID], [TypeName]) VALUES (2, 'Рефакторинг')
+INSERT INTO [dbo].[WorkTypes] ([ID], [TypeName]) VALUES (3, 'Консультации по телефону ')
+INSERT INTO [dbo].[WorkTypes] ([ID], [TypeName]) VALUES (4, 'Заплатка')
