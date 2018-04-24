db6095e42e0bf8deb6f811d9992485944581400e
Elena <a.lena.chur@gmail.com>
2018-04-24 20:46:14 +0300

    Изменения в бд и скрипте работ

--- DB_scripts/Test_Works_Тест012_script.sql
+++ DB_scripts/Test_Works_Тест012_script.sql
@@ -1,14 +1,14 @@
 USE[TaskManagmentDB]
 GO
 
-INSERT INTO [dbo].[Works] (WorkName, TaskID, WorkTypeID) VALUES ('Тестовая ошибка', 
+INSERT INTO [dbo].[Works] (WorkName, TaskID, WorkTypeID, Date) VALUES ('Тестовая ошибка', 
 	(SELECT [dbo].[Tasks].ID FROM [dbo].[Tasks]
-	WHERE [Tasks].[TaskName] = 'ГПН-Лаборатория'), 1)  
+	WHERE [Tasks].[TaskName] = 'ГПН-Лаборатория'), 1, '24-04-2018')  
 
-INSERT INTO [dbo].[Works] (WorkName, TaskID, WorkTypeID) VALUES ('Тест рефракторинг', 
+INSERT INTO [dbo].[Works] (WorkName, TaskID, WorkTypeID, Date) VALUES ('Тест рефракторинг', 
 	(SELECT [dbo].[Tasks].ID FROM [dbo].[Tasks]
-	WHERE [Tasks].[TaskName] = 'ГПН-Лаборатория'), 2) 
+	WHERE [Tasks].[TaskName] = 'ГПН-Лаборатория'), 2, '20-04-2018') 
 	
-INSERT INTO [dbo].[Works] (WorkName, TaskID, WorkTypeID) VALUES ('Тестовая Консультация по телефону', 
+INSERT INTO [dbo].[Works] (WorkName, TaskID, WorkTypeID, Date) VALUES ('Тестовая Консультация по телефону', 
 	(SELECT [dbo].[Tasks].ID FROM [dbo].[Tasks]
-	WHERE [Tasks].[TaskName] = 'ГПН-Лаборатория'), 3)   
+	WHERE [Tasks].[TaskName] = 'ГПН-Лаборатория'), 3, '27-04-2018')   
