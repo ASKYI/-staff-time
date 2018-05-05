2ead3db409d9e8466b3214afff6f82bc79c5dee5
Elena <a.lena.chur@gmail.com>
2018-05-06 00:47:53 +0300

    Создание интерфейсов в модели

---
+++ Staff-time/Staff-time/Model/Interfaces/ITaskWork.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Staff_time.Model.Interfaces
+{
+    public interface ITaskWork
+    {
+        void Create_Task(Task task);
+        void Create_TaskToFave(int taskID, int curUserID);
+        void Read_AllTasks();
+        void Read_FaveTasks(int curUser);
+        void Update_Task(Task task);
+        void Delete_Task(int taskID);
+        void Delete_TaskFromFave(int taskID);
+    }
+}
---
+++ Staff-time/Staff-time/Model/Interfaces/IWorkWork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Staff_time.Model.Interfaces
+{
+    public interface IWorkWork
+    {
+        void Create_Work(int taskID, Work work);
+        void Read_AllWorks();
+        void Read_WorksForDate(DateTime date));
+        void Read_WorksForTask(int taskID);
+        void Update_Work(Work work);
+        void Delete_Work(int workID);
+    }
+}
