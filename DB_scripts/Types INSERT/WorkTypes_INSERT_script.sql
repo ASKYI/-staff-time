4df39199a3e8208bfd8e5293b550ee57722602ee
Elena <a.lena.chur@gmail.com>
2018-05-06 00:39:36 +0300

    Коррекция в типах работ

--- DB_scripts/Types INSERT/WorkTypes_INSERT_script.sql
+++ DB_scripts/Types INSERT/WorkTypes_INSERT_script.sql
@@ -2,7 +2,7 @@ USE[TaskManagmentDB]
 GO
 
 INSERT INTO [dbo].[WorkTypes] ([ID], [TypeName]) VALUES (0, 'None')
-INSERT INTO [dbo].[WorkTypes] ([ID], [TypeName]) VALUES (1, 'Ошибка ')
+INSERT INTO [dbo].[WorkTypes] ([ID], [TypeName]) VALUES (1, 'Ошибка')
 INSERT INTO [dbo].[WorkTypes] ([ID], [TypeName]) VALUES (2, 'Рефакторинг')
 INSERT INTO [dbo].[WorkTypes] ([ID], [TypeName]) VALUES (3, 'Консультации по телефону ')
 INSERT INTO [dbo].[WorkTypes] ([ID], [TypeName]) VALUES (4, 'Заплатка')
