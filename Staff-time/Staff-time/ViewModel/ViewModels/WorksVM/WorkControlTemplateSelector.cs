a62eca95ee5721a7909b75cb4654589e1370ec49
Elena <a.lena.chur@gmail.com>
2018-05-10 01:42:55 +0300

    Отдельный контрол для работы Небольшие проблемы с привязкой DataContext через DataTemplate. Не хочу уходить в большой уровень вложенности. Либо найду решение, либо изучу свойства зависимостей.

--- Staff-time/Staff-time/ViewModel/ViewModels/WorksVM/WorkControlTemplateSelector.cs
+++ Staff-time/Staff-time/ViewModel/ViewModels/WorksVM/WorkControlTemplateSelector.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace Staff_time.ViewModel
 {
-    public class WorkControlTemplateSelector
+    public class WorkControlTemplateSelector : DataTemplateSelector
     {
+        public DataTemplate WorkTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+          //  if (item is WorkControlViewModel)
+                return WorkTemplate;
+
+      //      return null;
+        }
     }
 }
