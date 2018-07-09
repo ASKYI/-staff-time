1814394f4eb746a329a2362cc957784f36d5105e
Elena <a.lena.chur@gmail.com>
2018-07-09 13:41:10 +0300

    Добавлено контекстное меню для дерева, исправлен внешний вид панели с одной оставшейся кнопкой

--- Staff-time/Staff-time/View/TasksBlockView/TasksBlockView.xaml.cs
+++ Staff-time/Staff-time/View/TasksBlockView/TasksBlockView.xaml.cs
@@ -27,5 +27,39 @@ namespace Staff_time.View
             InitializeComponent();
             DataContext = context;
         }
+
+        private void TreeViewItem_PreviewMouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            context.ChangeSelection(((TreeViewItem)sender).DataContext as ViewModel.TreeNode);
+
+            ContextMenu menu = new ContextMenu();
+
+            MenuItem item = new MenuItem();
+            item.Header = "Добавить задачу рядом";
+            item.Command = context.AddNearTaskCommand;
+            menu.Items.Add(item);
+
+            item = new MenuItem();
+            item.Header = "Добавить подзадачу";
+            item.Command = context.AddChildTaskCommand;
+            menu.Items.Add(item);
+
+            item = new MenuItem();
+            item.Header = "Редактировать задачу";
+            item.Command = context.EditTaskCommand;
+            menu.Items.Add(item);
+
+            item = new MenuItem();
+            item.Header = "Удалить задачу";
+            item.Command = context.DeleteTaskCommand;
+            menu.Items.Add(item);
+
+            item = new MenuItem();
+            item.Header = "Добавить работу";
+            item.Command = context.AddWorkCommand;
+            menu.Items.Add(item);
+
+            (sender as TreeViewItem).ContextMenu = menu;
+        }
     }
 }
