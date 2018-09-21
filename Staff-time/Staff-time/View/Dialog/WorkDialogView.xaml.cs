1d5146da147595609b13a1fbfe27d304a2705f84
Elena <a.lena.chur@gmail.com>
2018-09-21 11:08:16 +0300

    View Dialog исправлено, кроме оставленного пока вопроса об отдельном месте для шаблона дерева

--- Staff-time/Staff-time/View/Dialog/WorkDialogView.xaml.cs
+++ Staff-time/Staff-time/View/Dialog/WorkDialogView.xaml.cs
@@ -8,7 +8,7 @@ namespace Staff_time.View
     /// <summary>
     /// Логика взаимодействия для WorkDialogView.xaml
     /// </summary>
-    public partial class WorkDialogView : Window, IDialogView //done: убраны лишнии функции, оставили Closing += (+ метод при закрытии)
+    public partial class WorkDialogView : Window, IDialogView
     {
         public WorkDialogView()
         {
@@ -20,6 +20,16 @@ namespace Staff_time.View
             InitializeComponent();
             DataContext = context;
             Closing += ((ViewModel.WorkDialogViewModel)DataContext).OnWindowClosing;
+        }
+
+        private void OK_Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Cancel_Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
